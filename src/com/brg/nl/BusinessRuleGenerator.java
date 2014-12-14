43261d1f1459dc1b34c73c5f9c43fe82a3ac83e0
floris <florvisguitar@gmail.com>
2014-12-14 21:38:32 +0100

    Third commit

---
+++ src/com/brg/nl/BusinessRuleGenerator.java
@@ -0,0 +1,25 @@
+package com.brg.nl;
+
+public class BusinessRuleGenerator {
+ private String name;
+ 
+ public BusinessRuleGenerator(String nm){
+	 name = nm;
+ }
+ 
+ public void generate(String lan, String repDB){
+	 
+ }
+ 
+ public void execute(){
+	 
+ }
+ 
+ public void addTrigger(){
+	 
+ }
+ 
+ public void checkTrigger(){
+	 
+ }
+}
---
+++ src/com/brg/nl/Language.java
@@ -0,0 +1,21 @@
+package com.brg.nl;
+public class Language {
+	private String name;
+ public Language(String nm){
+	name = nm;
+ }
+ 
+ public String getName(){
+	return name; 
+ }
+ 
+ public void setName(String nm){
+	 name = nm;
+ }
+ 
+ public boolean hasLanguage(String nm){
+	 boolean b = false;
+	 //vergelijk ingevoerde naam nm met alle namen die beschikbaar zijn in de library
+	 return b;
+ }
+}
