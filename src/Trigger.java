a88ee5b62d948fd7bbde26acf582b55d5595498f
Niels Wingelaar <niels_tvwo@live.nl>
2014-12-14 21:07:42 +0100

    new commit

---
+++ src/Trigger.java
@@ -0,0 +1,30 @@
+
+public class Trigger {
+ private String name;
+ private String path;
+ private boolean toBeExecuted;
+ 
+ public Trigger(String nm, String pt, boolean tbe){
+	 name = nm;
+	 path = pt;
+	 toBeExecuted = tbe;
+ }
+ 
+ public String read(){
+	 String s = "";
+	 
+	 return s;
+ }
+ 
+ public void write(){
+	 
+ }
+ 
+ public void createTrigger(){
+	 
+ }
+ 
+ public void check(){
+	 
+ }
+}
