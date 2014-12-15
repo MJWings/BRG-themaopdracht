b88fb95d0f7cd15416d6d2d67fd5ef2ccd720f27
floris <florvisguitar@gmail.com>
2014-12-15 12:39:53 +0100

    Commit 5

--- src/com/brg/nl/Main.java
+++ src/com/brg/nl/Main.java
@@ -6,10 +6,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		int value1 = 1;
 		int value2 = 10;
+		Trigger t1 = new Trigger("ARR_Trigger", "testPath", true);
 		
 		BusinessRuleGenerator BRG = new BusinessRuleGenerator("Business Rule Generator");
 		
-		BRG.addTrigger();
+		BRG.addTrigger("BRG_THOSAD_CIJFER_CNS_ARR_01","test_path", true);
 		BRG.execute();
 	}
 }
